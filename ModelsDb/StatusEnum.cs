eb0ce3de3bcab33bac0049bc8d65759a29d1b64a
Admin <Admin@DESKTOP-RR8A6QN>
2021-10-26 21:33:56 +0300

    Add project files.

---
+++ ModelsDb/StatusEnum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModelsDb
+{
+    public enum StatusEnum
+    {
+        //waiting for Consumer
+        Pending = 1,
+
+        //One of the Consumers is handling the task
+        InProgress = 2,
+
+        //Task failed to be handled
+        Error = 3,
+
+        //Task completed successfully
+        Done = 4
+    }
+}
---
+++ ModelsDb/Task.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ModelsDb
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public DateTime? CreationTime { get; set; }
+        public DateTime? ModificationTime { get; set; }
+        public string Status { get; set; }
+        public int ConsumerID { get; set; }
+    }
+}
